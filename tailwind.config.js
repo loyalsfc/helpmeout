31c41be28ea5fa0a1457ef3a3216d71c5666c52f
Olumide Bambe <loyalsfc@gmail.com>
2023-09-30 22:54:04 +0100

    Completed UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,25 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        "primary-50": "#E7E7ED",
+        "primary-100": "#B9C2C8",
+        "primary-200": "#928FAB",
+        "primary-300": "#605C84",
+        "primary-400": "#413C6D",
+        "primary-600": "#100A42",
+        "primary-800": "#0A0628",
+        "primary-900": "#08051E",
+        "primary-main": "#120B48",
+        "title-color": "#141414",
+        "grey": "#616163"
+      },
+      fontFamily: {
+        "sora": '[var(--sora-font)]',
+        "inter": '[var(--font-inter)]',
+        "sen": '[var(--sen-font)]',
+        "manrope": '[var(--font-manrope)]'
+      }
     },
   },
   plugins: [],
